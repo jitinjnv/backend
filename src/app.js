ac028fc8f15a4e605560fea9aaf9aa6334fdfe9e
jitin patel <jitin.21233@knit.ac.in>
2025-02-07 18:26:11 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
 const app=express()
 
 app.use(cors({
@@ -12,4 +13,9 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:'16kbb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
-export{app}
+
+// routes import
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users",userRouter)
+
+export {app}
--- src/utils/ayncHandler.js
+++
@@ -1,4 +0,0 @@
-const asynHandler=()=>{
-
-}
-export {asynHandler}
